c3b976ec2ae62bd56c2355ae18e5790a99ec7981
Shawn Schulz <shawnschulz@berkeley.edu>
2025-12-17 21:28:10 -0800

    Finally get tokenization working

--- data_handler.cpp
+++ data_handler.cpp
@@ -78,8 +78,12 @@ extern "C" {
             return;
         }
         for (auto const& p: reverse_vocab) {
-            file << p.first << p.second;
+            int length = p.first.size();
+            file.write(reinterpret_cast<const char*>(&length), sizeof(length));
+            file.write(p.first.data(), length);
+            file.write(reinterpret_cast<const char*>(&p.second), sizeof(p.second));
         }
+        file.close();
         return;
     }
 
@@ -92,16 +96,51 @@ extern "C" {
             return;
         }
         for (auto const& p: forward_vocab) {
-            file << p.first << p.second;
+            int length = p.second.size();
+            file.write(reinterpret_cast<const char*>(&p.first), sizeof(p.first));
+            file.write(reinterpret_cast<const char*>(&length), sizeof(length));
+            file.write(p.second.data(), length);
         }
+        file.close();
         return;
     }
 
     // Blegh just create some different function headers for the deserializtion methods we need
-    void deserialize_forward(const std::string& filename) {
+    std::unordered_map<int, std::string> deserialize_forward(const std::string& filename) {
+        std::unordered_map<int, std::string> forward_vocab;
+        std::ifstream file(filename, std::ios::binary);
+        int key;
+        int length;
+        if (!file.is_open()) {
+            std::cerr
+                << "[ERROR] Failed to open file for reading."
+                << std::endl;
+            return forward_vocab;
+        }
+        while (file && file.peek() != EOF) {
+            file.read(reinterpret_cast<char*>(&key), sizeof(key));
+            file.read(reinterpret_cast<char*>(&length), sizeof(length));
+            std::string value(length, '\0');
+            file.read(&value[0], length);
+            forward_vocab.insert({key, value});
+        }
+        return forward_vocab;
     }
 
-    void deserialize_reverse(const std::string& filename) {
+    std::unordered_map<std::string, int> deserialize_reverse(const std::string& filename) {
+        std::ifstream file(filename, std::ios::binary);
+        std::unordered_map<std::string, int> reverse_vocab;
+        std::string key;
+        int value;
+        while (file && file.peek() != EOF) {
+            int length;
+            file.read(reinterpret_cast<char*>(&length), sizeof(length));
+            std::string key(length, '\0');
+            file.read(&key[0], length);
+            file.read(reinterpret_cast<char*>(&value), sizeof(value));
+            reverse_vocab.insert({key, value});
+        }
+        return reverse_vocab;
     }
 
     // read column from the parquet file and tokenize it using some tokenization scheme so we can actually
@@ -109,7 +148,7 @@ extern "C" {
     // Use byte pair encoding. for now we'll just single thread this, since it's unlikely to be the bottlenck
     // and it's actually quite tricky to parallelize the BPE algorithm
     // but in the future we'll want a kernel for this too
-    void train_tokenizer(char* byte_characters, int input_size, int vocab_size) {
+    void train_tokenizer(std::string training_string, int vocab_size) {
         // Construct the vocabulary
         // these will be serilaizable and deseriable for easy encoding and decoding
         std::unordered_map<int, std::string> forward_vocab;
@@ -124,13 +163,14 @@ extern "C" {
         int n = 1;
         int n_vocab_found = 0;
         while ( n_vocab_found < vocab_size ) {
-            for (int i = 0; i < input_size; i++) {
-                if ( n + i < input_size - 1 ) {
-                    std::string string_window(std::next(byte_characters, i), std::next(byte_characters, n + i));
+            for (int i = 0; i < training_string.size(); i++) {
+                if ( n + i < training_string.size() - 1 ) {
+                    std::string string_window = training_string.substr(i, n + i);
                     if (frequencies.find(string_window) == frequencies.end()) {
                         frequencies.insert({string_window, 1});
                         if (n = 1) {
                             forward_vocab.insert({forward_vocab.size() + 1, string_window});
+                            reverse_vocab.insert({string_window, forward_vocab.size()});
                             n_vocab_found++;
                         }
                     }
@@ -140,22 +180,24 @@ extern "C" {
                     }
                 }
             }
+            // use a heap to add vocab of frequency > 1 so we can sort
+            std::priority_queue<std::pair<int, std::string>> vocab_queue;
+            for (auto& it: frequencies) {
+                if ((it.first.size() == 1) || (frequencies[it.first] > 1)) {
+                    if (reverse_vocab.find(it.first) != reverse_vocab.end()) {
+                        vocab_queue.push(std::make_pair(it.second, it.first));
+                    }
+                }
+            }
+            while ((!vocab_queue.empty()) && (forward_vocab.size() < vocab_size)) {
+                forward_vocab.insert({forward_vocab.size() + 1, vocab_queue.top().second});
+                reverse_vocab.insert({vocab_queue.top().second, forward_vocab.size()});
+                vocab_queue.pop();
+                n_vocab_found++;
+            }
             n++;
         }
 
-        // use a heap to add vocab of frequency > 1 so we can sort
-        std::priority_queue<std::pair<int, std::string>> vocab_queue;
-        for (auto& it: frequencies) {
-            if ((it.first.size() == 1) || (frequencies[it.first] > 1)) {
-                vocab_queue.push(std::make_pair(it.second, it.first));
-            }
-        }
-
-
-        while ((!vocab_queue.empty()) && (forward_vocab.size() < vocab_size)) {
-            forward_vocab.insert({forward_vocab.size() + 1, vocab_queue.top().second});
-            vocab_queue.pop();
-        }
 
         // Construct the reverse_vocab from the forward_vocab
         int counter = 0;
@@ -172,7 +214,7 @@ extern "C" {
     // i think the easiest way to start with this is to scan across the input string with a pointer
     // until you get the max length sub string that you can find in the lookup table
     // and add that as a token to the output int[] array
-    int* encode_tokens(std::vector<std::string>& forward_vocab, std::string input_string) {
+    int* encode_tokens(std::unordered_map<int, std::string>& forward_vocab, std::string input_string) {
     }
 
     // Uses the reverse lookup table created by train_tokenizer to detokenize output text
